2751ba1dcc5f63a0e0221043ee101ceb1e561e40
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-06 14:09:43 -0500

    Update cart.service.ts

--- bicimundo/src/app/services/cart.service.ts
+++ bicimundo/src/app/services/cart.service.ts
@@ -1,33 +1,57 @@
 
+
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
-  private carrito = new BehaviorSubject<any[]>(this.obtenerCarritoDesdeStorage());
+  private carrito = new BehaviorSubject<any[]>([]);
   carrito$ = this.carrito.asObservable();
 
-  private obtenerCarritoDesdeStorage(): any[] {
-    return JSON.parse(localStorage.getItem('carrito') || '[]');
+  constructor(private auth: AuthService) {
+    this.auth.user$.subscribe(user => {
+      const carritoGuardado = user 
+        ? JSON.parse(localStorage.getItem(`carrito_${user.email}`) || '[]')
+        : [];
+      this.carrito.next(carritoGuardado);
+    });
   }
+
 
   private saveCart(cart: any[]) {
-    localStorage.setItem('carrito', JSON.stringify(cart));
-    this.carrito.next(cart);
+    const user = this.auth.currentUser;
+    if (user && user.email) {
+      localStorage.setItem(`carrito_${user.email}`, JSON.stringify(cart));
+    } else {
+      localStorage.setItem('carrito_invitado', JSON.stringify(cart));
+    }
   }
 
-  agregarAlCarrito(producto: any) {
-    const nuevo = [...this.carrito.value, producto];
-    this.saveCart(nuevo);
+  agregarAlCarrito(producto: any, cantidad: number = 1) {
+    const cart = [...this.carrito.value];
+    const idx = cart.findIndex(item => item.id === producto.id);
+  
+    if (idx > -1) {
+      cart[idx].cantidad += cantidad;
+    } else {
+      cart.push({ ...producto, cantidad });
+    }
+  
+    this.carrito.next(cart); 
+    this.saveCart(cart);     
   }
+  
 
   eliminarDelCarrito(index: number) {
     const copia = [...this.carrito.value];
     copia.splice(index, 1);
-    this.saveCart(copia);
+    this.carrito.next(copia);    
+    this.saveCart(copia);        
   }
 
   clear() {
     this.saveCart([]);
   }
 }
+
