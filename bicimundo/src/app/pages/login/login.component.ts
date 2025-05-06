06058e4f143bf86948c22902694168d76799bcf4
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-06 14:06:26 -0500

    Update login.component.ts

--- bicimundo/src/app/pages/login/login.component.ts
+++ bicimundo/src/app/pages/login/login.component.ts
@@ -1,37 +1,52 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [ CommonModule, RouterModule, FormsModule ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
   email: string = '';
   password: string = '';
-  carrito: any[] = [];
+
+  constructor(private router: Router,
+    private auth: AuthService,
+  ) {}
 
   login() {
     const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
-    const usuario = usuarios.find((u: any) => u.email === this.email && u.password === this.password);
+    const usuario = usuarios.find((u: any) =>
+      u.email === this.email && u.password === this.password
+    );
 
     if (usuario) {
-      localStorage.setItem('usuarioActual', JSON.stringify(usuario));
-      alert(`Bienvenido, ${usuario.nombre} 👋`);
+      this.auth.setUser(usuario);
 
-      window.location.href = '/home';
+      Swal.fire({
+        position: 'top',
+        icon: 'success',
+        text: `Bienvenido, ${usuario.nombre} 👋`,
+        showConfirmButton: false,
+        timer: 1000
+      }).then(() => {
+        this.router.navigate(['/home']);
+      });
     } else {
-      alert('Correo o contraseña incorrectos');
+      Swal.fire({
+        position: 'top',
+        icon: 'error',
+        text: 'Correo o contraseña incorrectos.',
+        confirmButtonText: 'Volver a intentar.',
+        confirmButtonColor: '#e60023',
+      });
     }
-  }
-
-  cerrarSesion() {
-    localStorage.removeItem('usuarioActual');
-    window.location.href = '/login';
   }
 }
