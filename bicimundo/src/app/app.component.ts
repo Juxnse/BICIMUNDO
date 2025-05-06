7d87d2e9d70f69e2113eb37ad9826d58f4c99e23
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-05 22:22:01 -0500

    Update app.component.ts

--- bicimundo/src/app/app.component.ts
+++ bicimundo/src/app/app.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CartService } from './services/cart.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
+  
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, 
+    CommonModule, 
+    RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'bicimundo';
+  cartCount$!: Observable<number>;
+  usuarioActual: any = null;
+  currentRoute = '';
+  ocultarEnRutas = ['/login', '/registro'];
+
+
+  
+  
+  constructor(private router: Router,
+    private cartService: CartService,
+  ) {}
+
+
+  ngOnInit() {
+
+    this.cartCount$ = this.cartService.carrito$.pipe(
+      map(items => items.length)
+    );
+    const usuarioGuardado = localStorage.getItem('usuarioActual');
+    if (usuarioGuardado) {
+      this.usuarioActual = JSON.parse(usuarioGuardado);
+    }
+
+    this.router.events
+      .pipe(filter(evt => evt instanceof NavigationEnd))
+      .subscribe((evt: NavigationEnd) => {
+        this.currentRoute = evt.urlAfterRedirects;
+      });
+  }
+
+  shouldShowLayout(): boolean {
+    return !this.ocultarEnRutas.includes(this.currentRoute);
+  }
+
+  cerrarSesion() {
+    localStorage.removeItem('usuarioActual');
+    this.cartService.clear();
+    window.location.href = '/home';
+    
+  }
+
 }
