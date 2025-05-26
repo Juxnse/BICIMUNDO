ba9f0546acb4c9034a1f00c92fe0c53fee47ac55
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-25 22:06:59 -0500

    Update cart.service.ts

--- bicimundo/src/app/services/cart.service.ts
+++ bicimundo/src/app/services/cart.service.ts
@@ -1,15 +1,18 @@
 
-
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { AuthService } from './auth.service';
+import { Bicicleta, ProductService } from './product.service';
+import Swal from 'sweetalert2';
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
   private carrito = new BehaviorSubject<any[]>([]);
   carrito$ = this.carrito.asObservable();
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService,
+    private productService: ProductService,
+  ) {
     this.auth.user$.subscribe(user => {
       const carritoGuardado = user 
         ? JSON.parse(localStorage.getItem(`carrito_${user.email}`) || '[]')
@@ -28,30 +31,78 @@ export class CartService {
     }
   }
 
-  agregarAlCarrito(producto: any, cantidad: number = 1) {
-    const cart = [...this.carrito.value];
-    const idx = cart.findIndex(item => item.id === producto.id);
-  
-    if (idx > -1) {
-      cart[idx].cantidad += cantidad;
-    } else {
-      cart.push({ ...producto, cantidad });
+   agregarAlCarrito(producto: Bicicleta, cantidad: number = 1) {
+    if (!this.auth.currentUser) {
+      Swal.fire(
+        'Error',
+        'No se pueden agregar bicicletas al carrito si no estás logeado',
+        'error'
+      );
+      return;
     }
-  
-    this.carrito.next(cart); 
-    this.saveCart(cart);     
+
+    if (producto.stock < cantidad) {
+        Swal.fire('Sin stock', 'No hay stock disponible para éste producto por el momento, lamentamos las molestias.', 'warning',
+        );
+      return;
+    }
+
+    const updatedBike: Bicicleta = {
+      ...producto,
+      stock: producto.stock - cantidad
+    };
+    this.productService.update(updatedBike);
+
+    const cartClone = [...this.carrito.value];
+    const idx = cartClone.findIndex(item => item.id === producto.id);
+
+    if (idx > -1) {
+      cartClone[idx].cantidad += cantidad;
+    } else {
+      cartClone.push({ ...producto, cantidad });
+    }
+
+    this.carrito.next(cartClone);
+    this.saveCart(cartClone);
+
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      text: `Agregaste ${cantidad} x ${producto.nombre} al carrito.`,
+      showConfirmButton: false,
+      timer: 1000
+    });
   }
   
 
-  eliminarDelCarrito(index: number) {
-    const copia = [...this.carrito.value];
-    copia.splice(index, 1);
-    this.carrito.next(copia);    
-    this.saveCart(copia);        
+eliminarDelCarrito(index: number) {
+  const cartClone = [...this.carrito.value];
+
+  const [eliminado] = cartClone.splice(index, 1);
+
+
+  if (eliminado) {
+
+    const todasLasBicis = this.productService.getAll();
+
+    const biciEnStock = todasLasBicis.find(b => b.id === eliminado.id);
+    if (biciEnStock) {
+
+      const updatedBici: Bicicleta = {
+        ...biciEnStock,
+        stock: biciEnStock.stock + (eliminado.cantidad || 1)
+      };
+
+      this.productService.update(updatedBici);
+    }
   }
+
+
+  this.carrito.next(cartClone);
+  this.saveCart(cartClone);
+}
 
   clear() {
     this.saveCart([]);
   }
 }
-
