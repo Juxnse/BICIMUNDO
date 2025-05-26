eb20a75e47132f49a39759dba4daca42b40ee708
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-25 22:07:18 -0500

    Update auth.service.ts

--- bicimundo/src/app/services/auth.service.ts
+++ bicimundo/src/app/services/auth.service.ts
@@ -1,37 +1,44 @@
-
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-
+import { Router } from '@angular/router';
 
 export interface User {
-    id: number;
-    cedula: string;
-    email: string;
-    nombre: string;
-    fechaNacimiento: string;
-    password: string;
-    rol: string;
-  }
-  
+  id?: number;
+  cedula?: string;
+  email: string;
+  nombre: string;
+  fechaNacimiento?: string;
+  password: string;
+  rol: string;
+}
+
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    private userSubject = new BehaviorSubject<User|null>(
-        JSON.parse(localStorage.getItem('usuarioActual') || 'null')
-      )
+  private userSubject = new BehaviorSubject<User|null>(
+    JSON.parse(localStorage.getItem('usuarioActual') || 'null')
+  );
   user$: Observable<User|null> = this.userSubject.asObservable();
 
-  setUser(user: any) {
-    localStorage.setItem('usuarioActual', JSON.stringify(user));
-    this.userSubject.next(user);
-  }
+  constructor(private router: Router) {}
 
+setUser(user: User) {
+  localStorage.setItem('usuarioActual', JSON.stringify(user));
+  this.userSubject.next(user);
+
+  if (user.rol === 'admin') {
+    this.router.navigateByUrl('/admin', { replaceUrl: true });
+  } else {
+    this.router.navigateByUrl('/home', { replaceUrl: true });
+  }
+}
 
   clearUser() {
     localStorage.removeItem('usuarioActual');
     this.userSubject.next(null);
+    this.router.navigate(['/home']);
   }
 
-  get currentUser() {
+  get currentUser(): User|null {
     return this.userSubject.value;
   }
 }
