50d2ad89ee1714297d9991e115a81ffd0ba6056d
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-25 21:55:20 -0500

    Update bicicletas.component.ts

--- bicimundo/src/app/pages/bicicletas/bicicletas.component.ts
+++ bicimundo/src/app/pages/bicicletas/bicicletas.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
-import Swal from 'sweetalert2';
+import { ProductService, Bicicleta } from '../../services/product.service';
 
 
 @Component({
@@ -13,54 +13,27 @@ import Swal from 'sweetalert2';
   styleUrls: ['./bicicletas.component.css']
 })
 export class BicicletasComponent implements OnInit {
+  bicicletas: Bicicleta[] = [];
+  constructor(
+    private ps: ProductService,
+    private cart: CartService
+  ) {}
 
-  bicicletas = [
-    {
-      id: 1,
-      nombre: 'Urban Ride X',
-      descripcion: 'Bicicleta híbrida perfecta para trayectos urbanos y rutas de mediana distancia.',
-      precio: 750000,
-      imagen: 'bicicleta-roja-hibrida.jpg'
-    },
-    {
-      id: 2,
-      nombre: 'Mountain Pro 500',
-      descripcion: 'Ideal para la aventura en montaña, con suspensión reforzada y frenos hidráulicos.',
-      precio: 1200000,
-      imagen: 'mountain-pro-500.jpg'
-    },
-    {
-      id: 3,
-      nombre: 'Speedster 900',
-      descripcion: 'Diseñada para alta velocidad en carretera, con cuadro ligero y componentes aerodinámicos.',
-      precio: 950000,
-      imagen: 'speedster-900-pro.jpg'
-    }
-  ];
+  
 
   usuarioActual: any = null;
 
 
-  constructor(
-    private cartService: CartService,
-  ) {}
-
-
   ngOnInit() {
-    const usuarioGuardado = localStorage.getItem('usuarioActual');
-    if (usuarioGuardado) {
-      this.usuarioActual = JSON.parse(usuarioGuardado);
-    }
+    this.bicicletas = this.ps.getAll();
+    this.ps.bicicletas$.subscribe(list => this.bicicletas = list);
   }
 
-  agregarAlCarrito(bici: any) {
-    this.cartService.agregarAlCarrito(bici);
-    Swal.fire({
-      position: "top-end",
-      icon: "success",
-      text: `La bicicleta "${bici.nombre}" ha sido agregada al carrito.`,
-      showConfirmButton: false,
-      timer: 1000
-    });
+  agregarAlCarrito(bici: Bicicleta) {
+    if (!bici) return;
+
+    this.cart.agregarAlCarrito(bici);
+
   }
 }
+
