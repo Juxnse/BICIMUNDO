cf04c460b081ce3f57f20e34f8ec875a97549d3c
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-29 20:44:39 -0500

    Create interceptors for JWT

---
+++ bicimundo/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,29 @@
+// src/app/interceptors/auth.interceptor.ts
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      // Clona la petición y añade la cabecera Authorization
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(authReq);
+    }
+    return next.handle(req);
+  }
+}
