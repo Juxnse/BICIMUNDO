35c92a6fb64c54ad3106cb921c146bbc04705af6
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-29 20:38:28 -0500

    Update app.module.ts

--- bicimundo/bicimundo-backend-nest/src/app.module.ts
+++ bicimundo/bicimundo-backend-nest/src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
 import { ClientesModule } from './clientes/clientes.module';
 import { CarritosModule } from './carritos/carritos.module';
 import { CarritoItemsModule } from './carrito_items/carrito_items.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ProductosModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
     ClientesModule,
+    ProductosModule,
     CarritosModule,
-    CarritoItemsModule
+    CarritoItemsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
