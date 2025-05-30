9f2291402add2a02b4d3522f0bcaf508036fee55
VictorVidal12 <110042484+VictorVidal12@users.noreply.github.com>
2025-05-29 20:34:55 -0500

    Update clientes.module.ts

--- bicimundo/bicimundo-backend-nest/src/clientes/clientes.module.ts
+++ bicimundo/bicimundo-backend-nest/src/clientes/clientes.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { ClientesService } from './clientes.service';
 import { ClientesController } from './clientes.controller';
 import { SupabaseService } from '../supabase/supabase.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   controllers: [ClientesController],
   providers: [ClientesService, SupabaseService],
+  exports: [ClientesService],
 })
 export class ClientesModule {}
